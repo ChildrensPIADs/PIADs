330d504b09cb99212e76979f1f2d23593fb60bca
CullenMertens <31663160+CullenMertens@users.noreply.github.com>
2018-02-05 10:50:59 -0600

    update

--- untitled folder/Children's PIADs/Children's PIADs/ViewController.swift
+++ untitled folder/Children's PIADs/Children's PIADs/ViewController.swift
@@ -2,10 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var queationLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var forwardButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     
+    @IBOutlet weak var valueTest: UILabel!
     
     
     override func viewDidLoad() {
@@ -17,27 +18,55 @@ class ViewController: UIViewController {
     
     var questionValue = 1
     
+    func updateQuestionLabel() {
+        if questionValue == 2 {
+            questionLabel.text = " 2" }
+        else if questionValue == 3 {
+            questionLabel.text = "3" }
+        else if questionValue == 4 {
+            questionLabel.text = "3" }
+        else if questionValue == 5 {
+            questionLabel.text = "3" }
+        else if questionValue == 6 {
+            questionLabel.text = "3" }
+        else if questionValue == 7 {
+            questionLabel.text = "3" }
+        else if questionValue == 8 {
+            questionLabel.text = "3" }
+        else if questionValue == 9 {
+            questionLabel.text = "3" }
+        else if questionValue == 10 {
+            questionLabel.text = "3" }
+        else if questionValue == 11 {
+            questionLabel.text = "3" }
+        else if questionValue == 12 {
+            questionLabel.text = "3" }
+        else if questionValue == 13 {
+            questionLabel.text = "3" }
+        else if questionValue == 14 {
+            questionLabel.text = "3" }
+        else if questionValue == 15 {
+            questionLabel.text = "3" }
+        
+        }
+    }
+    
     @IBAction func forwardButtonPressed(_ sender: UIButton) {
-        questionValue += 1
+        questionValue = questionValue + 1
+        updateQuestionLabel()
         
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
         
         if questionValue > 1 {
-            questionValue -= 1
+            questionValue = questionValue - 1
+            updateQuestionLabel()
         }
         
-        func updateQuestionLabel() {
-            if questionValue == 1 {
-                queationLabel.text = "Makes it easier for me to do things." }
-            else if questionValue == 2 {
-                queationLabel.text = " 2" }
-            else if questionValue == 1 {
-                queationLabel.text = "3." }
-        }
+       
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
